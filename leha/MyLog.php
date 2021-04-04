c807889369cfb9b2e5d5d137c5189b492df419d9
Lexa <gaerkhanov@bk.ru>
2021-04-04 23:41:30 +0300

    Lab9

--- leha/MyLog.php
+++ leha/MyLog.php
@@ -18,9 +18,14 @@ Class MyLog extends LogAbstract implements LogInterface {
 	}
     
     public function _write(){
-        
+
+
+        $d = date_create();
+        $z = (string)$d->format('d-m-Y\TH.i.s.u');
         foreach($this->log as $value){
             echo $value."\n";
+
+            file_put_contents(__DIR__ . "\..\log\\".$z.".log", trim($value."\r\n") . PHP_EOL, FILE_APPEND);
         }
         
     }
